953fb1ff1639063d52c3958b08bb93b17028f3be
Mannar Hielal <mannar.hielal@liip.ch>
2021-05-09 11:37:24 +0200

    Add some rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "indent": ["error", 2], // 2 spaces
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"]
   },
 };
