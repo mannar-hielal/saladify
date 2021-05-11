d91e25a8335a21cf2ef1da2aab4ff1aeb103b251
Mannar Hielal <mannar.hielal@liip.ch>
2021-05-11 23:23:06 +0200

    Install fontawesome & libraries

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ import router from "./router";
 import store from "./store";
 import "./assets/styles/styles.css";
 import Vuelidate from "vuelidate";
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faFontAwesome, fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
 Vue.use(Vuelidate);
+
+library.add(faFontAwesome, fab, fas);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
