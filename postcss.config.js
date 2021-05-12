cf1f0344947d06102ad933336bbf12ced5ba9e1e
Mannar Hielal <mannar.hielal@liip.ch>
2021-05-12 17:05:41 +0200

    Update to postcss8 and install postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require("tailwindcss"), // we want this time to use tailwind as plugin
+    require("postcss-nested"),
     require("autoprefixer"), // a plugin that automatically adds vendor prefexier
   ],
 };
