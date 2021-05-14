025edb4992db613476c1326a6ea7d9a22d275eac
Mannar Hielal <mannar.hielal@liip.ch>
2021-05-14 09:03:14 +0200

    Install & config postcss extend rule

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("tailwindcss"), // we want this time to use tailwind as plugin
     require("postcss-nested"),
+    require("postcss-extend-rule"),
     require("autoprefixer"), // a plugin that automatically adds vendor prefexier
   ],
 };
