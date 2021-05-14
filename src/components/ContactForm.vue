49c9c9a6793771ed2f724ce954d1d015465c1457
Mannar Hielal <mannar.hielal@liip.ch>
2021-05-14 10:19:47 +0200

    Complete ContactForm

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="contact">
+    <div v-if="!submitted">
+      <h1 class="text-center">Get In Touch</h1>
+      <p class="my-8 text-center">
+        Lorem ipsum dolor sunt placeat velit quia Voluptate tenetur reiciendis
+        <br />
+        aliquam quaerat expedita Odio sed deserunt officia.
+      </p>
+      <form @submit.prevent="submitForm" class="card" id="contactForm">
+        <!-- name -->
+        <div class="form-item">
+          <label for="name" class="label">
+            <div class="icon-wrapper">
+              <font-awesome-icon :icon="['fas', 'user']" />
+            </div>
+            <input
+              type="text"
+              id="name"
+              :class="{
+                errorInput: $v.form.name.$error,
+                successInput: !$v.form.name.$invalid,
+              }"
+              v-model="form.name"
+              @input="$v.form.name.$touch()"
+              placeholder="Your name"
+            />
+          </label>
+          <!-- notification -->
+          <p
+            v-if="!$v.form.name.required && $v.form.name.$error"
+            class="errorMsg"
+          >
+            Name is required!
+          </p>
+
+          <p
+            v-if="!$v.form.name.minLength && $v.form.name.$error"
+            class="errorMsg"
+          >
+            Name must be valid!
+          </p>
+        </div>
+
+        <!-- email -->
+        <div class="form-item">
+          <label for="email" class="label">
+            <div class="icon-wrapper">
+              <font-awesome-icon :icon="['fas', 'envelope']" />
+            </div>
+
+            <input
+              type="email"
+              id="email"
+              v-model="form.email"
+              @input="$v.form.email.$touch()"
+              :class="{
+                errorInput: $v.form.email.$error,
+                successInput: !$v.form.email.$invalid && $v.form.email.$dirty,
+              }"
+              placeholder="your@email.com"
+            />
+          </label>
+          <!-- notification -->
+          <p
+            v-if="!$v.form.email.email && $v.form.email.$error"
+            class="errorMsg"
+          >
+            Please input a valid email address!
+          </p>
+          <p
+            v-if="!$v.form.email.required && $v.form.email.$error"
+            class="errorMsg"
+          >
+            Email is required!
+          </p>
+        </div>
+
+        <!-- message -->
+        <div class="form-item">
+          <label for="message" class="label">
+            <div class="icon-wrapper">
+              <font-awesome-icon :icon="['fas', 'pen-nib']" />
+            </div>
+            <textarea
+              name="message"
+              form="contactForm"
+              v-model="form.message"
+              @input="$v.form.message.$touch()"
+              placeholder="Enter your message here"
+              :class="{
+                errorInput: $v.form.message.$error,
+                successInput: !$v.form.message.$invalid,
+              }"
+              class="h-32"
+            ></textarea>
+          </label>
+          <!-- notification -->
+          <p
+            v-if="!$v.form.message.required && $v.form.message.$error"
+            class="errorMsg"
+          >
+            Message is required!
+          </p>
+
+          <p
+            v-if="!$v.form.message.minLength & $v.form.message.$error"
+            class="errorMsg"
+          >
+            Yout message must be at least 10 character
+          </p>
+        </div>
+
+        <!-- newsletter check -->
+        <div class="text-left form-item">
+          <input type="checkbox" id="newsletter" v-model="form.newsletter" />
+          <label for="newsletter">Subscribe to the newsletter</label>
+        </div>
+
+        <!-- submit button -->
+        <div class="form-item">
+          <button class="w-full btn btn--primary">
+            <font-awesome-icon :icon="['fas', 'paper-plane']" />
+            Send Message
+          </button>
+        </div>
+      </form>
+    </div>
+    <div v-else class="text-center form-item">
+      <h1>Thank you!</h1>
+      <p class="my-8">
+        I will get in touch with you when my cat allows me to work quietly :)
+        <button class="m-8 mx-auto btn btn--secondary">
+          <font-awesome-icon :icon="['fas', 'home']" />
+          Go to Homepage
+        </button>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+import {
+  required,
+  minLength,
+  email,
+  requiredIf,
+} from "vuelidate/lib/validators";
+export default {
+  name: "ContactForm",
+  data() {
+    return {
+      submitted: false,
+      form: {
+        name: null,
+        message: null,
+        email: null,
+        newsletter: null,
+      },
+    };
+  },
+  validations: {
+    form: {
+      name: {
+        required: required,
+        minLength: minLength(3),
+      },
+      message: {
+        required: required,
+        minLength: minLength(20),
+      },
+      email: {
+        email: email,
+        required: requiredIf(function () {
+          return !!this.form.newsletter;
+        }),
+      },
+    },
+  },
+  computed: {},
+  methods: {
+    submitForm() {
+      this.$v.form.$touch();
+      if (!this.$v.form.$invalid) {
+        console.log("form is successfuly submitted!");
+        this.submitted = true;
+      } else {
+        console.log("invalid form");
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,169 +1,19 @@
 <template>
-  <div class="contact">
-    <div v-if="!submitted">
-      <h1>Question</h1>
-      <p>Don't hesitate to get in touch</p>
-      <form @submit.prevent="submitForm" id="contactForm">
-        <div>
-          <!-- name -->
-          <div>
-            <input
-              type="text"
-              id="name"
-              aria-label="name"
-              v-model="form.name"
-              @input="$v.form.name.$touch()"
-              :class="{
-                error: $v.form.name.$error,
-                success: !$v.form.name.$invalid,
-              }"
-              placeholder="Your name"
-            />
-            <!-- notification -->
-            <p
-              v-if="!$v.form.name.required && $v.form.name.$error"
-              class="text-red-500"
-            >
-              Name ist required
-            </p>
-
-            <p
-              v-if="!$v.form.name.minLength && $v.form.name.$error"
-              class="text-red-500"
-            >
-              Name must be valid!
-            </p>
-          </div>
-
-          <div>
-            <input type="checkbox" id="newsletter" v-model="form.newsletter" />
-            <label for="newsletter">Subscribe to the newsletter</label>
-          </div>
-
-          <!-- email -->
-          <div>
-            <input
-              type="email"
-              id="email"
-              aria-label="email"
-              v-model="form.email"
-              @input="$v.form.email.$touch()"
-              :class="{
-                error: $v.form.email.$error,
-                success: !$v.form.email.$invalid && $v.form.email.$dirty,
-              }"
-              placeholder="Your email address"
-            />
-            <!-- notification -->
-            <p
-              v-if="!$v.form.email.email && $v.form.email.$error"
-              class="text-red-500"
-            >
-              Please input a valid email
-            </p>
-            <p
-              v-if="!$v.form.email.required && $v.form.email.$error"
-              class="text-red-500"
-            >
-              Email is required
-            </p>
-          </div>
-
-          <!-- message -->
-          <div>
-            <textarea
-              name="message"
-              form="contactForm"
-              aria-label="message"
-              v-model="form.message"
-              @input="$v.form.message.$touch()"
-              placeholder="Enter your message here"
-              :class="{
-                error: $v.form.message.$error,
-                success: !$v.form.message.$invalid,
-              }"
-            ></textarea>
-            <!-- notification -->
-            <p
-              v-if="!$v.form.message.required && $v.form.message.$error"
-              class="text-red-500"
-            >
-              Message is required
-            </p>
-
-            <p
-              v-if="!$v.form.message.minLength & $v.form.message.$error"
-              class="text-red-500"
-            >
-              Yout message must be at least 10 character
-            </p>
-          </div>
-
-          <div>
-            <button>Submit</button>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div v-else>
-      <h1>Thank you!</h1>
-      <p>
-        I will get in touch with you when my cat allows me to work quietly :)
-      </p>
-    </div>
+  <div>
+    <contact-form></contact-form>
   </div>
 </template>
 
 <script>
-import {
-  required,
-  minLength,
-  email,
-  requiredIf,
-} from "vuelidate/lib/validators";
+import ContactForm from "@/components/ContactForm";
 
 export default {
   name: "Contact",
+  components: {
+    ContactForm,
+  },
   data() {
-    return {
-      submitted: false,
-      form: {
-        name: null,
-        message: null,
-        email: null,
-        newsletter: null,
-      },
-    };
-  },
-  validations: {
-    form: {
-      name: {
-        required: required,
-        minLength: minLength(3),
-      },
-      message: {
-        required: required,
-        minLength: minLength(10),
-      },
-      email: {
-        email: email,
-        required: requiredIf(function () {
-          return !!this.form.newsletter;
-        }),
-      },
-    },
-  },
-  computed: {},
-  methods: {
-    submitForm() {
-      this.$v.form.$touch();
-      if (!this.$v.form.$invalid) {
-        console.log("form is successfuly submitted!");
-        this.submitted = true;
-      } else {
-        console.log("invalid form");
-      }
-    },
+    return {};
   },
 };
 </script>
